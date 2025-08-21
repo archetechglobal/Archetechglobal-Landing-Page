aac1cb23f093853da55459bdfc2be0b0e9fc7989
yash-reddy01 <yashreddy.official@gmail.com>
2025-08-20 19:07:28 -0500

    Added some changes

--- components/sections/Work.tsx
+++ components/sections/Work.tsx
@@ -16,7 +16,7 @@ export const Work = () => {
                   Take a look at some of our recent projects and see how we&apos;ve helped our clients achieve their goals.
                 </p>
               </div>
-              <div className='grid grid-cols-3'>
+              <div className='grid grid-row-3 md:grid-col-3 py-12 md:py-24'>
                 <PinContainer
         title="Portfolio of Yash"
         href="https://www.yash-reddy.com"
@@ -33,22 +33,6 @@ export const Work = () => {
           <div className="flex flex-1 w-full rounded-lg mt-4 bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500" />
         </div>
       </PinContainer>
-      {/* <PinContainer
-        title="/ui.aceternity.com"
-        href="https://twitter.com/mannupaaji"
-      >
-        <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
-          <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
-            Aceternity UI
-          </h3>
-          <div className="text-base !m-0 !p-0 font-normal">
-            <span className="text-slate-500 ">
-              Customizable Tailwind CSS and Framer Motion Components.
-            </span>
-          </div>
-          <div className="flex flex-1 w-full rounded-lg mt-4 bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500" />
-        </div>
-      </PinContainer>
       <PinContainer
         title="/ui.aceternity.com"
         href="https://twitter.com/mannupaaji"
@@ -96,7 +80,23 @@ export const Work = () => {
           </div>
           <div className="flex flex-1 w-full rounded-lg mt-4 bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500" />
         </div>
-      </PinContainer> */}
+      </PinContainer>
+      <PinContainer
+        title="/ui.aceternity.com"
+        href="https://twitter.com/mannupaaji"
+      >
+        <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
+          <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
+            Aceternity UI
+          </h3>
+          <div className="text-base !m-0 !p-0 font-normal">
+            <span className="text-slate-500 ">
+              Customizable Tailwind CSS and Framer Motion Components.
+            </span>
+          </div>
+          <div className="flex flex-1 w-full rounded-lg mt-4 bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500" />
+        </div>
+      </PinContainer>
               </div>
               <div className="flex justify-center mt-8">
               <Button variant="outline" className="gap-1 bg-transparent">
